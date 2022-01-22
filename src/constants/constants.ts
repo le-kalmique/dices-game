f26f2dcddd44fd3aa34fef10b801ec00f7565443
Renata <rdzhergalova@gmail.com>
2022-01-22 18:11:19 +0200

    FIX Change the amount of dices to 5

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 import { IDice } from "../types/interfaces";
 
-const defaultDices: IDice[] = [...new Array(6).fill(0)].map((_, i) => ({
+const defaultDices: IDice[] = [...new Array(5).fill(0)].map((_, i) => ({
   value: i + 1,
   saved: false,
 }));
